25ecfcc95cd1569a11d6e01b688dfe3537a14c00
MasumRaj <promotionbd321@gmail.com>
2024-06-12 17:29:26 +0600

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         defaultTransition: Transition.fadeIn,
         transitionDuration: const Duration(milliseconds: 200),
-        initialRoute: AppRoute.homeScreen,
+        initialRoute: AppRoute.splashScreen,
         navigatorKey: Get.key,
         getPages: AppRoute.routes,
       ),
