7bcda5de958330d9b640a1859a9df66d4840db86
MasumRaj <promotionbd321@gmail.com>
2024-06-14 11:17:55 +0600

    following tab bar added

--- lib/view/widgets/custom_widgets/custom_widgets.dart
+++ lib/view/widgets/custom_widgets/custom_widgets.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:final_movie/core/app_routes.dart';
 import 'package:final_movie/utils/app_colors/app_colors.dart';
 import 'package:final_movie/utils/app_icons/app_icons.dart';
 import 'package:final_movie/utils/app_strings/app_strings.dart';
@@ -9,6 +10,7 @@ import 'package:final_movie/view/widgets/custom_network_image/custom_network_ima
 import 'package:final_movie/view/widgets/custom_text/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class CustomWidgets {
   ///============================Custom Flowing=====================
@@ -448,11 +450,16 @@ class CustomWidgets {
       padding: const EdgeInsets.only(right: 13,top: 10),
       child: Stack(
         children: [
-          CustomNetworkImage(
-            borderRadius: BorderRadius.circular(13),
-            height: 106.h,
-            width: 142.w,
-            imageUrl: image,
+          GestureDetector(
+            onTap: (){
+              Get.toNamed(AppRoute.movieDetails);
+            },
+            child: CustomNetworkImage(
+              borderRadius: BorderRadius.circular(13),
+              height: 106.h,
+              width: 142.w,
+              imageUrl: image,
+            ),
           ),
           Positioned(
               right: 15,
