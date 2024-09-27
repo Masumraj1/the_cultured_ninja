fb72de7cfc57deb8e8878b93be1cb280b07595a5
bdCalling <promotionbd321@gmail.com]>
2024-09-27 17:30:52 +0600

    sing in completed

--- lib/view/screen/setting_screen/change_password_screen/change_password_screen.dart
+++ lib/view/screen/setting_screen/change_password_screen/change_password_screen.dart
@@ -1,6 +1,8 @@
+import 'package:final_movie/controller/genarel_controller/genarel_controller.dart';
 import 'package:final_movie/utils/app_colors/app_colors.dart';
 import 'package:final_movie/utils/app_strings/app_strings.dart';
 import 'package:final_movie/view/widgets/custom_button/custom_button.dart';
+import 'package:final_movie/view/widgets/custom_loader/custom_loader.dart';
 import 'package:final_movie/view/widgets/custom_text/custom_text.dart';
 import 'package:final_movie/view/widgets/custom_text_field/custom_text_field.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +12,8 @@ import 'package:get/get.dart';
 class ChangePasswordScreen extends StatelessWidget {
   ChangePasswordScreen({super.key});
   final formKey = GlobalKey<FormState>() ;
+
+  final GeneralController generalController = Get.find<GeneralController>();
   @override
   Widget build(BuildContext context) {
 
@@ -29,114 +33,124 @@ class ChangePasswordScreen extends StatelessWidget {
         backgroundColor: AppColors.blackDeep,
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-          child: Form(
-            // key: formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomText(
-                  text: AppStrings.currentPassword,
-                  color: AppColors.lightWhite,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w400,
-                  bottom: 10,
-                ),
+      body: Obx(
+        () {
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+              child: Form(
+                // key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomText(
+                      text: AppStrings.currentPassword,
+                      color: AppColors.lightWhite,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w400,
+                      bottom: 10,
+                    ),
 
-                ///<<<<========================================Current Password Field======================================
-                const CustomTextField(
-                  hintText: AppStrings.password,
-                  // textEditingController:profileController.currentPasswordController ,
-                  keyboardType: TextInputType.name,
-                  isPassword: true,
-                  fillColor: AppColors.fromRgb,
-                  fieldBorderColor: AppColors.fromRgb,
-                  // validator: (value) {
-                  //   if (value.isEmpty) {
-                  //     return AppStrings.fieldCantBeEmpty;
-                  //   } else if (value.length < 6 &&
-                  //       !AppStrings.passRegExp.hasMatch(value)) {
-                  //     return AppStrings.passwordValidatorString;
-                  //   } else {
-                  //     return null;
-                  //   }
-                  // },
-                ),
+                    ///<<<<========================================Current Password Field======================================
+                     CustomTextField(
+                      hintText: AppStrings.password,
+                      textEditingController:generalController.oldPasswordController ,
+                      // textEditingController:profileController.currentPasswordController ,
+                      keyboardType: TextInputType.name,
+                      isPassword: true,
+                      fillColor: AppColors.fromRgb,
+                      fieldBorderColor: AppColors.fromRgb,
+                      // validator: (value) {
+                      //   if (value.isEmpty) {
+                      //     return AppStrings.fieldCantBeEmpty;
+                      //   } else if (value.length < 6 &&
+                      //       !AppStrings.passRegExp.hasMatch(value)) {
+                      //     return AppStrings.passwordValidatorString;
+                      //   } else {
+                      //     return null;
+                      //   }
+                      // },
+                    ),
 
-                CustomText(
-                  text: AppStrings.newPassword,
-                  color: AppColors.lightWhite,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w400,
-                  bottom: 10,
-                  top: 27,
-                ),
+                    CustomText(
+                      text: AppStrings.newPassword,
+                      color: AppColors.lightWhite,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w400,
+                      bottom: 10,
+                      top: 27,
+                    ),
 
-                ///<<<<========================================NewPassword Field======================================
-                const CustomTextField(
-                  isPassword: true,
-                  fillColor: AppColors.fromRgb,
-                  fieldBorderColor: AppColors.fromRgb,
-                  // textEditingController: profileController.newPasswordController,
-                  keyboardType: TextInputType.name,
-                  hintText: AppStrings.password,
-                  // validator: (value) {
-                  //   if (value.isEmpty) {
-                  //     return AppStrings.fieldCantBeEmpty;
-                  //   } else if (value.length < 6 &&
-                  //       !AppStrings.passRegExp.hasMatch(value)) {
-                  //     return AppStrings.passwordValidatorString;
-                  //   } else {
-                  //     return null;
-                  //   }
-                  // },
-                ),
+                    ///<<<<========================================NewPassword Field======================================
+                     CustomTextField(
+                      isPassword: true,
+                      fillColor: AppColors.fromRgb,
+                      fieldBorderColor: AppColors.fromRgb,
+                      textEditingController: generalController.passwordController,
+                      keyboardType: TextInputType.name,
+                      hintText: AppStrings.password,
+                      // validator: (value) {
+                      //   if (value.isEmpty) {
+                      //     return AppStrings.fieldCantBeEmpty;
+                      //   } else if (value.length < 6 &&
+                      //       !AppStrings.passRegExp.hasMatch(value)) {
+                      //     return AppStrings.passwordValidatorString;
+                      //   } else {
+                      //     return null;
+                      //   }
+                      // },
+                    ),
 
-                CustomText(
-                  text: AppStrings.confirmPassword,
-                  color: AppColors.lightWhite,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w400,
-                  bottom: 10,
-                  top: 27,
-                ),
+                    CustomText(
+                      text: AppStrings.confirmPassword,
+                      color: AppColors.lightWhite,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w400,
+                      bottom: 10,
+                      top: 27,
+                    ),
 
-                ///<<<<========================================Confirm Password Field======================================
-                const CustomTextField(
-                  // textEditingController: profileController.confirmPasswordController,
-                  keyboardType: TextInputType.name,
-                  hintText: AppStrings.password,
-                  fillColor: AppColors.fromRgb,
-                  fieldBorderColor: AppColors.fromRgb,
-                  isPassword: true,
-                  // validator: (value) {
-                  //   if (value.isEmpty) {
-                  //     return AppStrings.fieldCantBeEmpty;
-                  //   } else if (value != profileController.newPasswordController.text) {
-                  //     return " Password should match";
-                  //   }
-                  //   return null;
-                  // },
-                ),
-                SizedBox(
-                  height: 150.h,
-                ),
+                    ///<<<<========================================Confirm Password Field======================================
+                     CustomTextField(
+                      textEditingController: generalController.confirmPasswordController,
+                      keyboardType: TextInputType.name,
+                      hintText: AppStrings.password,
+                      fillColor: AppColors.fromRgb,
+                      fieldBorderColor: AppColors.fromRgb,
+                      isPassword: true,
+                      // validator: (value) {
+                      //   if (value.isEmpty) {
+                      //     return AppStrings.fieldCantBeEmpty;
+                      //   } else if (value != profileController.newPasswordController.text) {
+                      //     return " Password should match";
+                      //   }
+                      //   return null;
+                      // },
+                    ),
+                    SizedBox(
+                      height: 150.h,
+                    ),
 
-                ///<<<<========================================Save And Update Button======================================
-                Center(
-                  child: CustomButton(
-                    fillColor: AppColors.buttonColor,
-                    width: double.infinity,
-                    onTap: () {},
-                    title: AppStrings.changePassword,
-                  ),
+                    ///<<<<========================================Save And Update Button======================================
+
+
+                  generalController.isChangeLoading.value?CustomLoader():
+                    Center(
+                      child: CustomButton(
+                        fillColor: AppColors.buttonColor,
+                        width: double.infinity,
+                        onTap: () {
+                          generalController.changePassword();
+                        },
+                        title: AppStrings.changePassword,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        }
       ),
     );
   }
