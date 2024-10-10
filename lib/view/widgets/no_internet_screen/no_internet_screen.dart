b95a276fbe9a3da597f27afd0e5d6a430690b338
bdCalling <promotionbd321@gmail.com]>
2024-10-10 10:17:29 +0600

    some changes

--- lib/view/widgets/no_internet_screen/no_internet_screen.dart
+++ lib/view/widgets/no_internet_screen/no_internet_screen.dart
@@ -1,4 +1,5 @@
 import 'package:final_movie/core/app_routes.dart';
+import 'package:final_movie/utils/app_colors/app_colors.dart';
 import 'package:final_movie/view/widgets/custom_button/custom_button.dart';
 import 'package:final_movie/view/widgets/custom_text/custom_text.dart';
 import 'package:flutter/material.dart';
@@ -13,27 +14,26 @@ class NoInternetScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Icon(Icons.wifi),
+              const Icon(Icons.wifi,color: AppColors.lightWhite,),
               const CustomText(
                   top: 40,
+                  fontSize: 16,
                   bottom: 40,
                   maxLines: 3,
-                  text:'No Internet'),
+                  text:'No Internet',color: AppColors.lightWhite,),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: CustomButton(
-                    fillColor:Colors.red,
+                    fillColor:AppColors.buttonColor,
                     title: "Try Again",
-                    onTap: () {
-                      // onTap();
-                      Get.toNamed(AppRoute.signInScreen);
-                    }),
+                    onTap: onTap),
               )
             ],
           ),
